12352f994914de33548e08046ff5464d6819efce
Steven Wen <73114262+loopfree@users.noreply.github.com>
2023-03-22 16:19:52 +0700

    feat: created global variable and angle function

---
+++ src/global.js
@@ -0,0 +1,25 @@
+
+function radToDeg(r) {
+    return r * 180 / Math.PI;
+}
+
+function degToRad(d) {
+    return d * Math.PI / 180;
+}
+
+var canvas = document.getElementById("canvas");
+console.log(canvas)
+var gl = canvas.getContext("webgl");
+
+var isShade = false;
+var objVertices = [];
+var objColors = [];
+var objNormals = [];
+var centerobjectmin = [0,0,0];
+var centerobjectmax = [0,0,0];
+
+var translation = [325, 225, 0];
+var rotation = [degToRad(15), degToRad(30), degToRad(0)];
+var camrotation = [degToRad(0), degToRad(0), degToRad(0)];
+var zoom = 1.0;
+var scale = [1, 1, 1];
